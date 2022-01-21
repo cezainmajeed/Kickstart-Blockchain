dbed6768ace2de6326da7ab0b85ef443b6396003
cezainmajeed <cezainmajeed0713@gmail.com>
2022-01-21 12:21:02 +0530

    finished remix testing

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,16 +1,20 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+
     struct Request {
         string description;
         address recipient;
         uint value;
         bool complete;
+        mapping(address => bool) approvals;
+        uint approvalsCount;
     }
 
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
     Request[] public requests;
 
     modifier restricted() {
@@ -21,11 +25,13 @@ contract Campaign {
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
+        approversCount=0;
     }
 
     function contribue() public payable {
         require(msg.value > minimumContribution);
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string description,address recipient,uint value) public restricted {
@@ -33,9 +39,27 @@ contract Campaign {
             description:description,
             recipient:recipient,
             value:value,
-            complete:false
+            complete:false,
+            approvalsCount:0
         });
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage temp = requests[index];
+        require(approvers[msg.sender]);
+        require(!temp.approvals[msg.sender]);
+
+        temp.approvals[msg.sender]=true;
+        temp.approvalsCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage temp = requests[index];
+        require(!temp.complete);
+        require(temp.approvalsCount > (approversCount/2));
+        temp.complete=true;
+        temp.recipient.transfer(temp.value);
     }
 
 }
